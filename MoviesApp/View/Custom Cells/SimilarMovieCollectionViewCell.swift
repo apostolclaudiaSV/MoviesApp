fa77ba2b9a83f6568407e52e3c39654d11a7593a
apostolclaudiaSV <claudia.apostol@softvision.com>
2022-12-09 17:01:10 +0200

    added custom cell for similar movies collection view

---
+++ MoviesApp/View/Custom Cells/SimilarMovieCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SimilarMovieCollectionViewCell.swift
+//  MoviesApp
+//
+//  Created by claudia.apostol on 12/9/22.
+//
+
+import UIKit
+
+class SimilarMovieCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func configure(with image: UIImage, rating: String, title: String) {
+        posterImage.image = image
+        ratingLabel.text = rating
+        titleLabel.text = title
+    }
+}
--- MoviesApp/View/Custom Views/SimilarMoviesView.swift
+++ MoviesApp/View/Custom Views/SimilarMoviesView.swift
@@ -9,7 +9,31 @@ import UIKit
 
 class SimilarMoviesView: BaseCustomView {
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    var moviee: Movie?
     func configure(with movie: Movie) {
+        moviee = movie
+        setupCollectionView()
+        collectionView.reloadData()
+    }
+    
+    func setupCollectionView() {
+        collectionView.register(UINib.init(nibName: "SimilarMovieCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "SimilarMovieCollectionViewCell")
         
     }
+}
+
+extension SimilarMoviesView: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return moviee?.details?.genres.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SimilarMovieCollectionViewCell", for: indexPath) as! SimilarMovieCollectionViewCell
+        
+        cell.configure(with: moviee!.posterImage!, rating: String(moviee!.rating), title: moviee!.title)
+        return cell
+    }
+    
+    
 }
