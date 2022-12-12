2790f30f478b211140645f11641fec911e55b24a
apostolclaudiaSV <claudia.apostol@softvision.com>
2022-12-12 11:38:27 +0200

    added swipe action on favorites table view to remove movie from favorites list

--- MoviesApp/Controller/FavoritesTableViewController.swift
+++ MoviesApp/Controller/FavoritesTableViewController.swift
@@ -13,4 +13,19 @@ class FavoritesTableViewController: BaseTableViewController {
     override var sortCriteria: SortCriteria { .none }
     override var shouldHideFavoriteButton: Bool { true }
     override var screenTitle: String { Text.favoritesMovies.text }
+    
+}
+
+extension FavoritesTableViewController {
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let movie = filteredMovies[indexPath.row]
+        let action = UIContextualAction(style: .normal,
+                                        title: "Remove") { [weak self] (action, view, completionHandler) in
+            self?.didChangeFavorite(movie: movie)
+            tableView.reloadData()
+            completionHandler(true)
+        }
+        action.backgroundColor = .red
+        return UISwipeActionsConfiguration(actions: [action])
+    }
 }
